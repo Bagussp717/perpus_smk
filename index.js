fb42a95336ac5a5a5c6fee7b7a63b317a1433cda
bagus <91867610+Bagussp717@users.noreply.github.com>
2024-03-24 22:15:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors =require ('cors')
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://perpuson.vercel.app"],
     methods: ["GET", "POST", "DELETE","PUT"],
     credentials: true,
   })
